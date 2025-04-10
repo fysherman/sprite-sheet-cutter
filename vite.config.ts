5019cd47a0299af6929d8027023a78bc6a43e9c4
KhanhBD4 <KhanhBD4@fpt.com>
2025-04-10 16:14:08 +0700

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/sprite-sheet-cutter/',
   plugins: [
     tailwindcss(),
   ],
