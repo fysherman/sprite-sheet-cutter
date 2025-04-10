380f34ecc5f9fdd8b99840e206dcdf5bad64ef12
KhanhBD4 <KhanhBD4@fpt.com>
2025-04-10 17:37:56 +0700

    chore: update

--- src/main.ts
+++ src/main.ts
@@ -6,29 +6,43 @@ declare global {
   }
 }
 
-let loadedImage: HTMLImageElement | null = null;
-const imageInput = document.getElementById("imageInput") as HTMLInputElement;
-const gridCanvas = document.getElementById("gridCanvas") as HTMLCanvasElement;
-const hiddenCanvas = document.getElementById(
-  "hiddenCanvas"
-) as HTMLCanvasElement;
-const sliceWidthInput = document.getElementById(
-  "sliceWidth"
-) as HTMLInputElement;
-const sliceHeightInput = document.getElementById(
-  "sliceHeight"
-) as HTMLInputElement;
-const preview = document.getElementById("preview") as HTMLDivElement;
+let loadedImage: HTMLImageElement | undefined;
+let file: File | undefined;
+let fileExtension: string | undefined;
+const imageInput = document.querySelector<HTMLInputElement>("#imageInput")!;
+const gridCanvas = document.querySelector<HTMLCanvasElement>("#gridCanvas")!;
+const hiddenCanvas =
+  document.querySelector<HTMLCanvasElement>("#hiddenCanvas")!;
+const sliceWidthInput =
+  document.querySelector<HTMLInputElement>("#sliceWidth")!;
+const sliceHeightInput =
+  document.querySelector<HTMLInputElement>("#sliceHeight")!;
+const preview = document.querySelector<HTMLDivElement>("#preview")!;
+const nameInput = document.querySelector<HTMLInputElement>("#name")!;
 
 imageInput.addEventListener("change", function (event: Event) {
-  const file = (event.target as HTMLInputElement)?.files?.[0];
-  console.log("🚀 ~ file:", file)
+  file = (event.target as HTMLInputElement)?.files?.[0];
 
   if (!file) return;
 
+  console.log("🚀 ~ file:", file);
+
+  nameInput.value = file.name
+    .split(".")
+    .filter((_, i, arr) => {
+      const isLast = i === arr.length - 1;
+
+      if (isLast) fileExtension = arr[i];
+
+      return !isLast;
+    })
+    .join(".");
+
   const reader = new FileReader();
+
   reader.onload = function (e) {
     const img = new Image();
+
     img.onload = function () {
       loadedImage = img;
       drawGrid(); // Draw grid immediately
@@ -87,6 +101,7 @@ function sliceImage() {
 
   const cols = Math.ceil(loadedImage.width / sliceW);
   const rows = Math.ceil(loadedImage.height / sliceH);
+  const baseName = nameInput.value;
 
   for (let y = 0; y < rows; y++) {
     for (let x = 0; x < cols; x++) {
@@ -107,9 +122,13 @@ function sliceImage() {
       imgElem.src = dataUrl;
       imgElem.width = 100;
 
+      const name = `${baseName}_${(x + 1) * (y + 1)}`;
       const a = document.createElement("a");
+
       a.href = dataUrl;
-      a.download = `slice_${x}_${y}.png`;
+      a.download = `${name}.${fileExtension}`;
+      a.innerHTML = name;
+      a.style.maxWidth = `${sliceW}px`;
 
       div.appendChild(imgElem);
       div.appendChild(a);
@@ -119,11 +138,12 @@ function sliceImage() {
 }
 
 function download() {
-  document.querySelectorAll<HTMLLinkElement>('#preview a').forEach((a) => {
+  document.querySelectorAll<HTMLLinkElement>("#preview a").forEach((a) => {
     a.click();
-  })
+  });
 }
 
 window.sliceImage = sliceImage; // Expose the function to the global scope
 window.drawGrid = drawGrid; // Expose the function to the global scope
 window.download = download; // Expose the function to the global scope
+
